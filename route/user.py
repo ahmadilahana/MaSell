0317034d2631e5ab60df83a0f45a91b5fccaec36
ragabayu <ragabayukrisna@gmail.com>
2021-07-27 16:22:54 +0700

    validation phone

--- route/user.py
+++ route/user.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request
-from schemas.user import User
+from fastapi.exceptions import HTTPException
+from sqlalchemy.sql.expression import false
+from schemas.user import User, ValidPhoneNumber
 from config.db import conn
 from models.main import users
 
@@ -13,24 +15,35 @@ async def read_user():
 async def read_user_by_id(userId: int):
     return conn.execute(users.select().where(users.c.userId == userId)).fetchall()
 
+# @user.get("/users/getId/")
+# async def read_user_by(user= 1231421421):
+#     phonn= ValidPhoneNumber(user)
+#     return phonn
+
 @user.post("/users/add")
 async def create_user(user: User, request: Request):
-    return conn.execute(users.insert().values(
-        firsName= user.firsName,
-        lastName= user.lastName,
-        email= user.email,
-        avatarUrl= user.avatarUrl,
-        countryId= user.countryId,
-        countryCode= user.countryCode,
-        country= user.country,
-        verifyCode= user.verifyCode,
-        registerAt= user.registerAt,
-        deviceId= user.deviceId,
-        IpAddress= request.client.host,
-        isActived= user.isActived,
-        password= user.password
-    ))
-    return conn.execute(users.select()).fetchall()
+    validPhone= ValidPhoneNumber(user.phone)
+    if validPhone == True:
+        return conn.execute(users.insert().values(
+            firsName= user.firsName,
+            lastName= user.lastName,
+            email= user.email,
+            avatarUrl= user.avatarUrl,
+            countryId= user.countryId,
+            countryCode= user.countryCode,
+            country= user.country,
+            verifyCode= user.verifyCode,
+            registerAt= user.registerAt,
+            deviceId= user.deviceId,
+            IpAddress= request.client.host,
+            isActived= user.isActived,
+            password= user.password,
+            phone= user.phone
+        ))
+        return conn.execute(users.select()).fetchall()
+    else:
+        raise HTTPException(status_code=404)
+        return "Number Phone is Not valid"
 
 @user.put("/users/update{userId}")
 async def update_user(id: int, user: User):
@@ -47,7 +60,8 @@ async def update_user(id: int, user: User):
         deviceId= user.deviceId,
         IpAddress= user.IpAddress,
         isActived= user.isActived,
-        password= user.password
+        password= user.password,
+        phone= user.phone
     ).where(users.c.userId == userId))
     return conn.execute(users.select()).fetchall()
 
--- schemas/user.py
+++ schemas/user.py
@@ -1,19 +1,74 @@
+from typing import List, Optional
+from fastapi.param_functions import Depends
+from fastapi.params import Query
+import phonenumbers
 from pydantic import BaseModel
+from pydantic.fields import Field
 from datetime import datetime
-from fastapi import Request
+from fastapi import Request, Security
+from phonenumbers import PhoneNumber
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+import re
+
+from sqlalchemy.sql.expression import false
+
+class RealStr(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v): 
+               
+        if type(v) != str:            
+            try:
+                if type(v) == float:
+                    v = str(int(v))
+                else:
+                    v = str(v)
+                
+            except:
+                raise ValueError("Not a valid String")    
+        if not isinstance(v, str):
+            raise ValueError("Not a valid String")
+        if not v:
+            return str(v)
+            
+        if bool(re.compile('^[a-zA-Z\d\.\,\&\-\_\()\'\\/@\s]+$').match(v)):
+            return str(v)
+        else:
+            print(v)
+            raise ValueError(str(v) + "unallowed character")
 
 
+def ValidPhoneNumber(phone_number: RealStr):
+    my_number = phonenumbers.parse(phone_number, "ID")
+    validPhone = phonenumbers.is_valid_number(my_number)
+    
+    if not validPhone:
+        return false
+
+    if 7 < len(phone_number) < 14:
+        rule = re.compile(r'^(?:\+?44)?[07]\d{9,13}$')
+        if not rule.search(phone_number):            
+            return false
+        else:
+            return phone_number.isnumeric()
+    else:
+        return false
 
 class User(BaseModel):
-    firsName: str
-    lastName: str
-    email: str
-    avatarUrl: str
-    countryId: int
-    countryCode: str
-    country: str
-    verifyCode: int
-    registerAt: int
-    deviceId: int
-    isActived: int
-    password: str
+    firsName: RealStr = None
+    lastName: RealStr = None
+    email: RealStr = None
+    avatarUrl: RealStr = None
+    countryId: int = None
+    countryCode: RealStr = None
+    country: RealStr = None
+    verifyCode: int = None
+    registerAt: int = None
+    deviceId: int = None
+    isActived: int = None
+    password: str = None
+    phone: RealStr = None#Field(None, max_length=15, regex="^[-+]?[0-9]+$" )
